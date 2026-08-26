078f8962c0f5b41ca6c2841dff4f4174b023a1b0
ayanami <cwb511@outlook.com.com>
2026-08-26 13:00:09 +0800

    增加了适用于色彩空间的宏工程

---
+++ pixel-derive/src/lib.rs
@@ -0,0 +1,40 @@
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use regex::Regex;
+use syn::{Ident, ItemStruct, Token, parse::Parser, parse_macro_input, punctuated::Punctuated};
+
+#[proc_macro_attribute]
+pub fn color_space(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemStruct);
+    let name = input.ident; // 结构体名
+    let pixel_name = format_ident!("{name}Pixel");
+
+    let attr_str = parse_macro_input!((attr.clone()) as Ident).to_string();
+    let re = Regex::new(r"[\s*,\s*]").unwrap();
+    let pixel_names: Vec<Ident> = Punctuated::<Ident, Token![,]>::parse_terminated
+        .parse(attr)
+        .unwrap()
+        .into_iter()
+        .collect();
+    let channel = pixel_names.len();
+
+    // 生成实现代码
+    let expanded = quote! {
+        impl ColorSpace for #name {
+            const CHANNEL: usize = #channel;
+            type PixelType = #pixel_name;
+        }
+
+        #[repr(C)]
+        #[derive(Debug, Clone, Copy, Default, bytemuck::Pod, bytemuck::Zeroable)]
+        struct #pixel_name{
+            #(
+                #pixel_names: u8,
+            )*
+        }
+
+        impl Pixel for #pixel_name{}
+    };
+
+    TokenStream::from(expanded)
+}
--- src/color_space/rgb.rs
+++ src/color_space/rgb.rs
@@ -1,7 +1,9 @@
-use crate::{color_space::ColorSpace, pixel::Pixel};
+use pixel_derive::color_space;
 
 /// RGB通道类型
+#[color_space(r,g,b)]
 pub struct Rgb;
+/*pub struct Rgb;
 #[repr(C)]
 #[derive(Debug, Clone, Copy, Default, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct RgbPixel{
@@ -14,4 +16,4 @@ impl Pixel for RgbPixel{}
 impl ColorSpace for Rgb {
     const CHANNEL: usize = 3;
     type PixelType = RgbPixel;
-}
+}*/
