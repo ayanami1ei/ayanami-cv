1e4eadc4c97b07ed6d489b2330bc2608de66e3ba
ayanami <cwb511@outlook.com.com>
2026-08-27 08:52:25 +0800

    增加了roi

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod color_space;
 pub mod image;
 pub mod pixel;
 pub mod roi;
+pub mod point;
 
 pub use {crate::color_space::Gray, crate::image::image::Image};
---
+++ src/roi.rs
@@ -0,0 +1,47 @@
+use crate::color_space::ColorSpace;
+use crate::image::ImageViewLike;
+use crate::image::iter::pixel_iter::ImagePixelIter;
+use crate::image::iter::row_iter::ImageRowIter;
+use crate::point::Point;
+
+pub struct Roi<'a, C: ColorSpace> {
+    left_up:Point,
+    right_down:Point,
+    data: Vec<&'a [C::PixelType]>,
+}
+
+impl<'a, C: ColorSpace> ImageViewLike<C> for Roi<'a, C> {
+    fn width(&self) -> usize {
+        self.right_down.x-self.left_up.x
+    }
+    fn height(&self) -> usize {
+        self.right_down.y-self.left_up.y
+    }
+    fn pixel<'b>(&'b self)->&'b [<C as ColorSpace>::PixelType] {
+        self.data
+    }
+    fn data(&self) -> &[u8] {
+        bytemuck::cast_slice(&self.data)
+    }
+    fn row_iter(&self) -> ImageRowIter<'_, C> {
+        ImageRowIter::new(self)
+    }
+    fn pixel_iter(&self) -> ImagePixelIter<'_, C> {
+        ImagePixelIter::new(&self.data, self.height() * self.width())
+    }
+    fn at(&self, index: (usize, usize)) -> &C::PixelType {
+        &self.data[index.0 * self.height() + index.1]
+    }
+}
+
+impl<'a, C: ColorSpace> Roi<'a, C> {
+    pub fn new_from_image<I:ImageViewLike<C>>(image: &'a I, left_up:Point, right_down:Point) -> Self {
+        let mut data=Vec::new();
+        
+        Self {
+            left_up,
+            right_down,
+            data,
+        }
+    }
+}
