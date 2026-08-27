301510e116d3f20c757792a5594499445213109f
ayanami <cwb511@outlook.com>
2026-08-27 15:14:03 +0800

    新增 filter-derive 包及相关依赖，优化 MeanFilter 结构体实现

--- filter-derive/src/lib.rs
+++ filter-derive/src/lib.rs
@@ -1,19 +1,23 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{Ident, ItemStruct, Token, parse::Parser, parse_macro_input, punctuated::Punctuated};
+use quote::{quote};
+use syn::{ItemStruct,  parse_macro_input};
 
 #[proc_macro_attribute]
-pub fn filter(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn filter(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
     let name = input.ident; // 结构体名
-    let pixel_name = format_ident!("{name}Pixel");
 
     // 生成实现代码
     let expanded = quote! {
+        #[derive(Debug, Clone, Copy)]
         pub struct #name;
         impl #name {
-            pub fn filter(){
-                
+            pub fn filter<I: ImageViewLike<Gray>, IMut: ImageViewMutLike<Gray>, const SIZE: usize>(
+                &self,
+                src: &I,
+                dst: &mut IMut,
+            ) -> Result<(), Error> {
+                neighborhood::<I, IMut, SIZE, #name>(src, dst, *self)
             }
         }
     };
--- src/algorithm/neighborhood/filters.rs
+++ src/algorithm/neighborhood/filters.rs
@@ -1,10 +1,13 @@
+use filter_derive::filter;
+
 use crate::{
     Gray, GrayPixel, ImageViewLike, ImageViewMutLike,
     algorithm::neighborhood::{NeighborhoodAlgorithm, WindowLike, error::Error, neighborhood},
 };
 
-#[derive(Debug, Clone, Copy)]
+#[filter]
 pub struct MeanFilter;
+
 impl NeighborhoodAlgorithm for MeanFilter {
     fn process<W: WindowLike<Gray>>(&self, window: &W) -> GrayPixel {
         let range = (W::SIZE / 2) as i32;
@@ -15,14 +18,5 @@ impl NeighborhoodAlgorithm for MeanFilter {
             }
         }
         GrayPixel { gray: (sum / (W::SIZE * W::SIZE) as u64) as u8 }
-    }
-}
-impl MeanFilter {
-    pub fn filter<I: ImageViewLike<Gray>, IMut: ImageViewMutLike<Gray>, const SIZE: usize>(
-        &self,
-        src: &I,
-        dst: &mut IMut,
-    ) -> Result<(), Error> {
-        neighborhood::<I, IMut, SIZE, MeanFilter>(src, dst, *self)
     }
 }
